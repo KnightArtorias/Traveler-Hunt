2f3b313e5732286676d50242518a7c23cbafbafe
KnightArtorias <KnightArtorias@users.noreply.github.com>
2017-11-03 10:47:07 -0700

    Initial Commit

---
+++ Traveler Hunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Traveler Hunt
+//
+//  Created by Drew Nibeck on 11/3/17.
+//  Copyright © 2017 CSE310. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
